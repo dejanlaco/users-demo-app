c74177cd2b074bc27a220574ee1e8cb1c556c41f
Dejan Lacmanovic <dejanlacmanovic@Dejans-MacBook-Pro.local>
2017-06-23 21:15:25 +0200

    Added navmenu with shrink function

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,6 +10,8 @@ module.exports = function(defaults) {
     }
   });
 
+  app.import('vendor/custom.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //

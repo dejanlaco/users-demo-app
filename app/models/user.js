65a9714c0fb4af07e5dd6b9c77f5590e36d4589d
Dejan Lacmanovic <dejanlacmanovic@Dejans-MacBook-Pro.local>
2017-06-23 10:46:47 +0200

    Added points field to users

--- app/models/user.js
+++ app/models/user.js
@@ -3,5 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	fname: DS.attr('string'),
 	lname: DS.attr('string'),
-	email: DS.attr('string')
+	email: DS.attr('string'),
+	points: DS.attr('number', {
+		defaultValue() { return 0; }
+	})
 });
